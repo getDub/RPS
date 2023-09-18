bb26bca378409f4a0fd519a13146ee94ceb3d7bd
getDub <143089345+getDub@users.noreply.github.com>
2023-09-18 12:23:19 +1000

    Game plays through once
    The three blocks of code work and play through once and log the choices and result to console.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,22 +1,24 @@
-//playerSeleciton inputs eithe rock, paper or scissors.
-//Paremeter must be case sensitive
 
-function game() {
 
+//function game() {
 
 
 
-function getPlayerChoice() {
     
-    
+    function getPlayerChoice() {
+        
+        
     const playerInput = prompt (`Choose either Rock, Paper or Scissors`);
     let pSFormatted = playerInput.toLowerCase();
     let frontLetter = pSFormatted.slice(0,1).toUpperCase();
     let wordLength = pSFormatted.slice(1, );
-    return`${frontLetter}${wordLength}`;
+    let playersChoice = `${frontLetter}${wordLength}`;
+    console.log(playersChoice);
+    return playersChoice
 }
 
-console.log(getPlayerChoice());
+getPlayerChoice
+//console.log(getPlayerChoice);
 
 
 
@@ -27,19 +29,22 @@ function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     
     let random = Math.floor(Math.random() * 3);
-   
-    return choices.at(random);
+   let computersChoice = choices.at(random);
+   console.log(computersChoice);
+    return computersChoice
     
     
 };
 
-console.log(getComputerChoice());
+getComputerChoice
+//console.log(getComputerChoice());
 
 //A function that take playerSelection, computerSelection and evaluates against each other
 //then declares a winner of the round.
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(/*playerSelection, computerSelection*/){
+    
     
     playerSelection = getPlayerChoice()
     computerSelection = getComputerChoice()
@@ -62,11 +67,15 @@ function playRound(playerSelection, computerSelection){
     }  else if (playerSelection != 'Rock' || playerSelection != "Paper" || playerSelection != "Scissors") {
         return `Your choise is outside the scope of this game. Please choose Rock, Paper or Scissors`;
     }
+
 };
 
+//playRound()
 console.log(playRound());
 
+//console.log(playRound());
 
-}
 
-game()
+//}
+
+//game()
