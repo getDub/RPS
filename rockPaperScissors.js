ed91ee44b912b9c8720b1d04a0cc460d29ebb457
getDub <143089345+getDub@users.noreply.github.com>
2023-09-19 13:21:03 +1000

    Game now iterates score
    Declared variables outside of the game scope so the counter++ would register a cycle through the code and iterate each time.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,12 +1,16 @@
+let computerCount = 0;
+let yourCount = 0;
+
+game()
+game()
+game()
+game()
+game()
 
 
-//function game() {
+function game() {
 
-
-
-    
     function getPlayerChoice() {
-        
         
     const playerInput = prompt (`Choose either Rock, Paper or Scissors`);
     let pSFormatted = playerInput.toLowerCase();
@@ -18,64 +22,59 @@
 }
 
 getPlayerChoice
-//console.log(getPlayerChoice);
 
 
-
-//A function for computerSelection choosing rock, paper or scissors randomly.
 
 function getComputerChoice() {
     
     const choices = ['Rock', 'Paper', 'Scissors'];
     
     let random = Math.floor(Math.random() * 3);
-   let computersChoice = choices.at(random);
-   console.log(computersChoice);
+    let computersChoice = choices.at(random);
+    console.log(computersChoice);
     return computersChoice
-    
     
 };
 
 getComputerChoice
-//console.log(getComputerChoice());
-
-//A function that take playerSelection, computerSelection and evaluates against each other
-//then declares a winner of the round.
 
 
-function playRound(/*playerSelection, computerSelection*/){
-    
+
+function playRound(){
     
     playerSelection = getPlayerChoice()
     computerSelection = getComputerChoice()
-
-
+    
+    
     if (playerSelection === computerSelection) {
         return `It's a tie. You chose ${playerSelection} and the computer chose ${computerSelection}`;
     } else if (playerSelection === "Rock" && computerSelection === `Paper`) {
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}`;
+        computerCount++;
+        return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${yourCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Rock" && computerSelection === `Scissors`) {
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}`;
+        yourCount++
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${yourCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Paper" && computerSelection === `Scissors`) {
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}`;
+        computerCount++;
+        return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${yourCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Paper" && computerSelection === `Rock`) {
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}`;
+        yourCount++
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${yourCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Scissors" && computerSelection === `Rock`) {
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}`;
+        computerCount++;
+        return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${yourCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Scissors" && computerSelection === `Paper`) {
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}`;
+        yourCount++
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${yourCount} and computer ${computerCount}.`;
     }  else if (playerSelection != 'Rock' || playerSelection != "Paper" || playerSelection != "Scissors") {
         return `Your choise is outside the scope of this game. Please choose Rock, Paper or Scissors`;
     }
 
 };
 
-//playRound()
+
 console.log(playRound());
 
-//console.log(playRound());
 
+}
 
-//}
-
-//game()
