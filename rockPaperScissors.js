5d12d3ac734f47e53f24e58187040fd1265399b2
getDub <143089345+getDub@users.noreply.github.com>
2023-10-09 22:38:01 +1100

    Game keeps score correctly and announces first to 5 winner.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -10,29 +10,29 @@ const winner = document.querySelector('.winner');
 const gameScore = document.querySelector('.gameStatus');
 
 let playerCount = 0;
-const pC = ++playerCount;
+//const pC = ++playerCount;
 let computerCount = 0;
-const cC = ++computerCount;
-announceWinner();
+//const cC = ++computerCount;
+
 let playerSelection;
 
 buttons.forEach((button) => 
 button.addEventListener('click', () => {
-
+    
     playerSelection = button.textContent;
     playersHand.textContent = `Player Choice: ${playerSelection} `;
-    computersHand.textContent = `Computers Choice: ${getComputerChoice()} `;
-    playerScore.textContent = `Player Score = ${playerCount}`;
-    computerScore.textContent = `Computer Score = ${computerCount}`;
+    //computersHand.textContent = `Computers Choice: ${computerSelection} `;
+    //playerScore.textContent = `Player Score = ${playerCount}`;
+    //computerScore.textContent = `Computer Score = ${computerCount}`;
     winner.textContent = `Announce Winner: ${announceWinner()}`;
     gameScore.textContent = `${playRound()}`;
     
     //playRound();
-        
+    
 }));
 
-    
-    
+
+
 function getComputerChoice() {
     
     const choices = ['Rock', 'Paper', 'Scissors'];
@@ -45,34 +45,47 @@ function getComputerChoice() {
 };
 
 
+
 function playRound(){
     
     computerSelection = getComputerChoice();
-    
+    computersHand.textContent = `Computers Choice: ${computerSelection} `;
+
     if (playerSelection === computerSelection) {
         return `It's a tie. You chose ${playerSelection} and the computer chose ${computerSelection}`;
     } else if (playerSelection === "Rock" && computerSelection === `Paper`) {
         ++computerCount;
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You loose. Computers ${computerSelection} beats your ${playerSelection}.`;
     } else if (playerSelection === "Rock" && computerSelection === `Scissors`) {
         ++playerCount;
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
     } else if (playerSelection === "Paper" && computerSelection === `Scissors`) {
         ++computerCount;
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You loose. Computers ${computerSelection} beats your ${playerSelection}.`;
     } else if (playerSelection === "Paper" && computerSelection === `Rock`) {
         ++playerCount;
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
     } else if (playerSelection === "Scissors" && computerSelection === `Rock`) {
         ++computerCount;
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You loose. Computers ${computerSelection} beats your ${playerSelection}.`;
     } else if (playerSelection === "Scissors" && computerSelection === `Paper`) {
         ++playerCount;
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
     }  else if (playerSelection != 'Rock' || playerSelection != "Paper" || playerSelection != "Scissors") {
         return `Your choice is outside the scope of this game. Please choose Rock, Paper or Scissors`;
     }
-    
     
 };
 
