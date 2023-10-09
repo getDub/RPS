0e71ca13e7b2bb5cdc924eefb97fce6cc4067d75
getDub <143089345+getDub@users.noreply.github.com>
2023-10-09 21:44:32 +1100

    Keeps incorrect score and announces game results and Winner after 5 game win.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,80 +2,47 @@
 
 
 const buttons = document.querySelectorAll('button');
-//const playerPressed = document.querySelectorAll('.Paper');
+const playersHand = document.querySelector('.playerChoice');
+const computersHand = document.querySelector('.compuChoice');
+const playerScore = document.querySelector('.playerScore');
+const computerScore = document.querySelector('.compuScore');
+const winner = document.querySelector('.winner');
+const gameScore = document.querySelector('.gameStatus');
 
-
+let playerCount = 0;
+const pC = ++playerCount;
+let computerCount = 0;
+const cC = ++computerCount;
+announceWinner();
 let playerSelection;
-
-//function getPlayerChoice (){
-//    //pressedBtn = button.textContent
-//    console.log(pressedBtn)
-//    return pressedBtn
-//};
 
 buttons.forEach((button) => 
 button.addEventListener('click', () => {
 
     playerSelection = button.textContent;
-    console.log(playerSelection);
-    //getComputerChoice()
-    playRound();
+    playersHand.textContent = `Player Choice: ${playerSelection} `;
+    computersHand.textContent = `Computers Choice: ${getComputerChoice()} `;
+    playerScore.textContent = `Player Score = ${playerCount}`;
+    computerScore.textContent = `Computer Score = ${computerCount}`;
+    winner.textContent = `Announce Winner: ${announceWinner()}`;
+    gameScore.textContent = `${playRound()}`;
     
+    //playRound();
+        
 }));
-//buttons.addEventListener('click', getPlayerChoice);
 
-
-
-
-let computerCount = 0;
-let playerCount = 0;
-announceWinner();
-//game();     //round 1
-
-
-
-
-//function game() {
-
-
-    //function getPlayerChoice() {
-        
-        //const playerInput = prompt (`Choose either Rock, Paper or Scissors`);
-        //let pSFormatted = playerInput.toLowerCase();
-        //let frontLetter = pSFormatted.slice(0,1).toUpperCase();
-        //let wordLength = pSFormatted.slice(1, );
-        //let playersChoice = `${frontLetter}${wordLength}`;
-        //const choices = ['Rock', 'Paper', 'Scissors'];
-        
-        
-    //let playersChoice = 'Rock';  
-     //   console.log(playersChoice)
-    //return playersChoice
-        
-    //};
-
-    //getPlayerChoice;
     
     
- 
-
-
-
-
-
 function getComputerChoice() {
     
     const choices = ['Rock', 'Paper', 'Scissors'];
     
     let random = Math.floor(Math.random() * 3);
     let computersChoice = choices.at(random);
-    console.log(computersChoice);
+    
     return computersChoice;
     
 };
-
-//getComputerChoice;
-
 
 
 function playRound(){
@@ -85,46 +52,37 @@ function playRound(){
     if (playerSelection === computerSelection) {
         return `It's a tie. You chose ${playerSelection} and the computer chose ${computerSelection}`;
     } else if (playerSelection === "Rock" && computerSelection === `Paper`) {
-        computerCount++;
+        ++computerCount;
         return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Rock" && computerSelection === `Scissors`) {
-        playerCount++;
+        ++playerCount;
         return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Paper" && computerSelection === `Scissors`) {
-        computerCount++;
+        ++computerCount;
         return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Paper" && computerSelection === `Rock`) {
-        playerCount++;
+        ++playerCount;
         return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Scissors" && computerSelection === `Rock`) {
-        computerCount++;
+        ++computerCount;
         return `You loose. Computers ${computerSelection} beats your ${playerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
     } else if (playerSelection === "Scissors" && computerSelection === `Paper`) {
-        playerCount++;
+        ++playerCount;
         return `You Win. Your ${playerSelection} beats computers ${computerSelection}. Scores are, You ${playerCount} and computer ${computerCount}.`;
     }  else if (playerSelection != 'Rock' || playerSelection != "Paper" || playerSelection != "Scissors") {
         return `Your choice is outside the scope of this game. Please choose Rock, Paper or Scissors`;
     }
-
+    
+    
 };
-
-//console.log(playRound());
-
-
-//}
-
-
 
 function announceWinner (){
     
-    if (computerCount < playerCount){
+    if (playerCount === 5 && computerCount < playerCount){
         return `Congratulations! You Win! ${playerCount} to ${computerCount}.`;
-    } else if (computerCount > playerCount){
+    } else if (computerCount === 5 &&  computerCount > playerCount){
         return `Computer wins. ${computerCount} to ${playerCount}. Better luck next time.`;
-    } else if (computerCount === playerCount){
-        return`It's a tie. ${playerCount} to ${computerCount}. Play agian.`;
-    }
-
+    } 
 };
 
-//console.log(announceWinner());
+
