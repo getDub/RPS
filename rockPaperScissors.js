f4dc60b0e97458be5a7419cfb20a22d554ca3e00
getDub <143089345+getDub@users.noreply.github.com>
2023-10-10 21:18:02 +1100

    Game creates a button after 5 games that when clicked reloads the page to start game again
    The score is still off. The iterator is off between declaration and 1 of the functions. It would be great if the button created could be like an alert button.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,94 +8,129 @@ const playerScore = document.querySelector('.playerScore');
 const computerScore = document.querySelector('.compuScore');
 const winner = document.querySelector('.winner');
 const gameScore = document.querySelector('.gameStatus');
+const btnDiv = document.querySelector('.buttons');
 
-let playerCount = 0;
-//const pC = ++playerCount;
-let computerCount = 0;
-//const cC = ++computerCount;
 
+//(function ()
+//{    
 let playerSelection;
 
 buttons.forEach((button) => 
-button.addEventListener('click', () => {
-    
+button.addEventListener('click', () => 
+{    
     playerSelection = button.textContent;
     playersHand.textContent = `Player Choice: ${playerSelection} `;
-    //computersHand.textContent = `Computers Choice: ${computerSelection} `;
-    //playerScore.textContent = `Player Score = ${playerCount}`;
-    //computerScore.textContent = `Computer Score = ${computerCount}`;
-    winner.textContent = `Announce Winner: ${announceWinner()}`;
+    winner.textContent = `${announceWinner()}`;
     gameScore.textContent = `${playRound()}`;
-    
-    //playRound();
     
 }));
 
+let playerCount = 0;
+let computerCount = 0;
 
 
-function getComputerChoice() {
+function announceWinner ()
+{
+    if (playerCount   === 5)
+    {      
+        const restart = document.createElement('div');
+        restart.textContent = 'Game Over';
+        btnDiv.appendChild(restart);
+        console.log(restart)
+        restart.addEventListener('click', () => {
+            location.reload();
+        });
+        
     
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    
-    let random = Math.floor(Math.random() * 3);
-    let computersChoice = choices.at(random);
-    
-    return computersChoice;
-    
-};
-
-
-
-function playRound(){
-    
-    computerSelection = getComputerChoice();
-    computersHand.textContent = `Computers Choice: ${computerSelection} `;
-
-    if (playerSelection === computerSelection) {
-        return `It's a tie. You chose ${playerSelection} and the computer chose ${computerSelection}`;
-    } else if (playerSelection === "Rock" && computerSelection === `Paper`) {
-        ++computerCount;
-        playerScore.textContent = `Player Score = ${playerCount}`;
-        computerScore.textContent = `Computer Score = ${computerCount}`;
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}.`;
-    } else if (playerSelection === "Rock" && computerSelection === `Scissors`) {
-        ++playerCount;
-        playerScore.textContent = `Player Score = ${playerCount}`;
-        computerScore.textContent = `Computer Score = ${computerCount}`;
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
-    } else if (playerSelection === "Paper" && computerSelection === `Scissors`) {
-        ++computerCount;
-        playerScore.textContent = `Player Score = ${playerCount}`;
-        computerScore.textContent = `Computer Score = ${computerCount}`;
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}.`;
-    } else if (playerSelection === "Paper" && computerSelection === `Rock`) {
-        ++playerCount;
-        playerScore.textContent = `Player Score = ${playerCount}`;
-        computerScore.textContent = `Computer Score = ${computerCount}`;
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
-    } else if (playerSelection === "Scissors" && computerSelection === `Rock`) {
-        ++computerCount;
-        playerScore.textContent = `Player Score = ${playerCount}`;
-        computerScore.textContent = `Computer Score = ${computerCount}`;
-        return `You loose. Computers ${computerSelection} beats your ${playerSelection}.`;
-    } else if (playerSelection === "Scissors" && computerSelection === `Paper`) {
-        ++playerCount;
-        playerScore.textContent = `Player Score = ${playerCount}`;
-        computerScore.textContent = `Computer Score = ${computerCount}`;
-        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
-    }  else if (playerSelection != 'Rock' || playerSelection != "Paper" || playerSelection != "Scissors") {
-        return `Your choice is outside the scope of this game. Please choose Rock, Paper or Scissors`;
+        return `Congratulations! You Win! ${playerCount} to ${computerCount}.`;
+    } 
+    else if (computerCount  === 5 )
+    {
+        const restart = document.createElement('button');
+        restart.textContent = 'Game Over';
+        btnDiv.appendChild(restart);
+        console.log(restart)
+        restart.addEventListener('click', () => {
+            location.reload();
+        });
+       
+        return `Computer wins. ${computerCount} to ${playerCount}. Better luck next time.`;
+    } 
+    else 
+    {
+        return `Game in progress`;
     }
     
 };
 
-function announceWinner (){
-    
-    if (playerCount === 5 && computerCount < playerCount){
-        return `Congratulations! You Win! ${playerCount} to ${computerCount}.`;
-    } else if (computerCount === 5 &&  computerCount > playerCount){
-        return `Computer wins. ${computerCount} to ${playerCount}. Better luck next time.`;
-    } 
+
+function getComputerChoice()
+{    
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    let random = Math.floor(Math.random() * 3);
+    let computersChoice = choices.at(random);
+    return computersChoice;
 };
 
 
+function playRound()
+{    
+    computerSelection = getComputerChoice();
+    computersHand.textContent = `Computers Choice: ${computerSelection} `;
+
+    if (playerSelection === computerSelection) 
+    {
+        return `It's a tie. You chose ${playerSelection} and the computer chose ${computerSelection}`;
+    } 
+    else if (playerSelection === "Rock" && computerSelection === `Paper`) 
+    {
+        computerCount++;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You loose. Computers' ${computerSelection} beats your ${playerSelection}.`;
+    } 
+    else if (playerSelection === "Rock" && computerSelection === `Scissors`) 
+    {
+        playerCount++;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
+    } 
+    else if (playerSelection === "Paper" && computerSelection === `Scissors`) 
+    {
+        computerCount++;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You loose. Computers' ${computerSelection} beats your ${playerSelection}.`;
+    } 
+    else if (playerSelection === "Paper" && computerSelection === `Rock`) 
+    {
+        playerCount++;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
+    } else if (playerSelection === "Scissors" && computerSelection === `Rock`) 
+    {
+        computerCount++;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You loose. Computers' ${computerSelection} beats your ${playerSelection}.`;
+    } 
+    else if (playerSelection === "Scissors" && computerSelection === `Paper`) 
+    {
+        playerCount++;
+        playerScore.textContent = `Player Score = ${playerCount}`;
+        computerScore.textContent = `Computer Score = ${computerCount}`;
+        return `You Win. Your ${playerSelection} beats computers ${computerSelection}.`;
+    }  
+    else if (playerSelection != 'Rock' || playerSelection != "Paper" || playerSelection != "Scissors") 
+    {
+        return `Your choice is outside the scope of this game. Please choose Rock, Paper or Scissors`;
+    } 
+};
+
+//clear scores
+//clear choices
+//stop game being played by removing 
+
+//}());
